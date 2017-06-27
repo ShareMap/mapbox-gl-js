894b25fa118abfe724884d758bfab5bc660c022a
John Firebaugh <john.firebaugh@gmail.com>
2017-06-26 18:08:24 -0700

    Simplify array typing

--- src/style-spec/function/index.js
+++ src/style-spec/function/index.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const expressions = require('./expressions');
 const compileExpression = require('./compile');
 const convert = require('./convert');
-const {ColorType, StringType, NumberType, ValueType, array, vector} = require('./types');
+const {ColorType, StringType, NumberType, ValueType, array} = require('./types');
 const serialize = require('./type_check').serialize;
 
 function createFunction(parameters, propertySpec) {
@@ -81,10 +81,7 @@ function getExpectedType(spec) {
     };
 
     if (spec.type === 'array') {
-        if (typeof spec.length === 'number')
-            return array(types[spec.value], spec.length);
-        else
-            return vector(types[spec.value] || ValueType);
+        return array(types[spec.value] || ValueType, spec.length);
     }
 
     return types[spec.type];
